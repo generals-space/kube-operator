826aa9b6dbe041d6ba7a41834454edeb7a4278b3
genearl <generals.space@gmail.com>
2021-04-07 23:10:10 +0800

    apiserver-boot init

---
+++ pkg/apis/doc.go
@@ -0,0 +1,11 @@
+
+
+
+
+//go:generate apiregister-gen --input-dirs ./... -h ../../boilerplate.go.txt
+
+//
+// +domain=generals.space
+
+package apis
+
---
+++ pkg/openapi/doc.go
@@ -0,0 +1,6 @@
+
+
+
+//go:generate openapi-gen-o . --output-package ../../pkg/openapi --report-filename violations.report -i ../../pkg/apis/...,../../vendor/k8s.io/api/core/v1,../../vendor/k8s.io/apimachinery/pkg/apis/meta/v1 -h ../../boilerplate.go.txt
+package openapi
+
