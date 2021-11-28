a7bcaf33646a1f65b090af213cddb30db24e598e
genearl <generals.space@gmail.com>
2021-11-28 21:01:30 +0800

    新增: 索引器的使用示例

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"path/filepath"
 	"time"
@@ -17,9 +16,8 @@ import (
 	"k8s.io/klog"
 )
 
-/*
-	各种处理函数中, obj即为watch接口响应得到的资源对象.
-*/
+////////////////////////////////////////////////////////////////////////////////
+// 各种处理函数中, obj即为watch接口响应得到的资源对象.
 
 func onAdd(obj interface{}) {
 	pod := obj.(*corev1.Pod)
@@ -41,6 +39,31 @@ func onDelete(obj interface{}) {
 	klog.Infof("delete a pod: +v", pod.Name)
 }
 
+////////////////////////////////////////////////////////////////////////////////
+var NodeNameIndex = "nodeName"
+
+// NodeNameIndexFunc 按主机名称对 pod 列表进行索引查询, 即查询指定主机上的 Pod 列表.
+//
+// cache缓存, indexer索引器等都是针对单个资源而言的, 不同资源的缓存/索引规则无法通用.
+//
+// 	@param obj: client-go 会在向 cache 中添加目标资源对象时, 调用索引器函数,
+//              截取该对象中的某一字段作为索引键.
+//
+// 如果把 Pod 看成是一张数据表, NodeName 为表中的一个字段的话, 就相当于对 NodeName 这一列创建索引.
+// 而其带来的效果就是, 查询操作时如果以 NodeName 为条件的话, 效率将大大提升, 
+// 但同时也会影响增/删速度.
+//
+// 	@return: 函数器函数的返回值格式是固定的, 必须是一个 []string.
+func NodeNameIndexFunc(obj interface{}) ([]string, error) {
+	pod, ok := obj.(*corev1.Pod)
+	if !ok {
+		return []string{}, nil
+	}
+	return []string{pod.Spec.NodeName}, nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 func main() {
 	home := homedir.HomeDir()
 	kubeconfig := filepath.Join(home, ".kube", "config")
@@ -48,7 +71,8 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// 初始化 client
+
+	klog.Infof("初始化 kube client...")
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		log.Panic(err.Error())
@@ -57,31 +81,38 @@ func main() {
 	stopCh := make(chan struct{})
 	defer close(stopCh)
 
-	klog.Infof("初始化 informer...")
+	klog.Infof("初始化 informer factory...")
+
 	// Shared指的是多个 lister 共享同一个cache, 而且资源的变化会同时通知到cache和listers.
-	factory := informers.NewSharedInformerFactory(clientset, 60 * time.Second)
+	factory := informers.NewSharedInformerFactory(clientset, 60*time.Second)
 
 	// podInformer 拥有两个方法: Informer, Lister.
 	// 其实可以把 Informer 看作是 watch 操作.
 	podInformer := factory.Core().V1().Pods()
-	informer := podInformer.Informer()
+	nodeInformer := factory.Core().V1().Nodes()
 	defer runtime.HandleCrash()
+
+	myIndexers := cache.Indexers{
+		// namespace 是 client-go 内置的默认索引器.
+		// 但是这里不能写, 因为会和原来的造成冲突(AddIndexers()实现中会与其合并)
+		// cache.NamespaceIndex: cache.MetaNamespaceIndexFunc,
+		NodeNameIndex:        NodeNameIndexFunc,
+	}
+	err = podInformer.Informer().AddIndexers(myIndexers)
+	if err != nil {
+		klog.Errorf("添加自定义索引器失败: %s", err)
+	}
 
 	// 启动 informer, 开始 list & watch 流程(不需要使用 go func() 模式)
 	factory.Start(stopCh)
-
-	// 从 apiserver 同步某种资源的全部对象, 即 list.
+	// 从 apiserver 同步某种资源的全部对象.
 	// 之后就可以使用watch这种资源, 维护这份缓存.
-	if !cache.WaitForCacheSync(stopCh, informer.HasSynced) {
-		errTimeout := fmt.Errorf("初次同步缓存超时失败")
-		runtime.HandleError(errTimeout)
-		return
-	}
+	factory.WaitForCacheSync(stopCh)
 
 	// 使用自定义 handler, 处理 watch 响应的各种事件.
 	// 具体的维护操作在informer内部执行, 这里挂载的是额外的触发器.
 	// 需要注意的是, 在上面的list过程中, 会不断触发onAdd事件, 相当于服务发现了.
-	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
 		AddFunc:    onAdd,
 		UpdateFunc: onUpdate,
 		DeleteFunc: onDelete,
@@ -89,15 +120,21 @@ func main() {
 
 	// 从informer对象创建lister, 不过这里的代码没有特殊的目的,
 	// 应该只是展示一下通过informer的接口得到list资源的方法.
-	podLister := podInformer.Lister()
 	// 从 lister 中获取所有 items
-	podList, err := podLister.List(labels.Everything())
+	nodeList, err := nodeInformer.Lister().List(labels.Everything())
 	if err != nil {
-		klog.Errorf("获取Pod列表失败: %s", err)
+		klog.Errorf("获取Node列表失败: %s", err)
 	}
-	klog.Infof("获取Pod列表")
-	for _, pod := range podList {
-		klog.Infof("%s", pod.Name)
+	klog.Infof("获取Node列表成功")
+	for _, node := range nodeList {
+		klog.Infof("Node: %s ======================", node.Name)
+		podsOnTheNode, err := podInformer.Informer().GetIndexer().ByIndex(NodeNameIndex, node.Name)
+		if err != nil {
+			klog.Errorf("查询Node: %s 索引失败: %s", node.Name, err)
+		}
+		for _, pod := range podsOnTheNode {
+			klog.Infof(pod.(*corev1.Pod).Name)
+		}
 	}
 
 	<-stopCh
--- readme.md
+++ readme.md
@@ -1,10 +1,25 @@
 # kube-operator
 
+参考文章
+
+1. [client-go系列之4---Indexer](https://zhuanlan.zhihu.com/p/266512431)
+    - 其中 2.1.1 节的索引器数据结构值得一看.
+2. [client-go Indexer索引器](https://herbguo.gitbook.io/client-go/informer#4.2-indexer-suo-yin-qi)
+    - index 的使用示例...有点难懂
+3. [client-go 之 Indexer 的理解](https://blog.51cto.com/u_15077560/2584555)
+    - 这篇文章的 indexer 示例代码比参考文章2的易懂.
+
 kuber: 1.16.2
 
-可以说, 这个示例就是最简的 controller 了, 不需要创建 CRD 资源和对应的 golang 对象(包含 GVK, Spec, Status等), 只监听 kuber 内置的资源对象(本工程中为`Node`主机资源)的变动.
+本示例实现了按主机名称对 pod 列表进行索引查询, 即查询指定主机上的 Pod 列表.
 
-`informer`存在的意义就是, 在kuber集群各组件在与apiserver进行通信时添加一个中间缓存层, 减轻apiserver的负载压力. `informer`内置缓存功能, 可保证与apiserver查询到的数据保持一致.
+> cache缓存, indexer索引器等都是针对单个资源而言的, 不同资源的缓存/索引规则无法通用.
+
+client-go 会在向 cache 中添加目标资源对象时, 调用索引器函数, 截取该对象中的某一字段作为索引键.
+
+如果把 Pod 看成是一张数据表, NodeName 为表中的一个字段的话, 就相当于对 NodeName 这一列创建索引.
+
+而其带来的效果就是, 查询操作时如果以 NodeName 为条件的话, 效率将大大提升, 但同时也会影响增/删速度.
 
 ## 运行
 
@@ -12,7 +27,22 @@ kuber: 1.16.2
 
 ```console
 $ go run main.go
-I0330 18:14:15.093787   36338 main.go:55] 初始化 informer...
-I0330 18:14:17.194311   36338 main.go:25] add a node:k8s-master-01
-I0330 18:14:17.194299   36338 main.go:93] 获取主机列表: [&Node{ObjectMeta:{k8s-master-01   /api/v1/nodes/k8s-master-01...
+I1128 20:38:46.972581    4236 main.go:63] 初始化 kube client...
+I1128 20:38:46.995989    4236 main.go:72] 初始化 informer factory...
+I1128 20:38:47.601481    4236 main.go:120] 获取Node列表成功
+I1128 20:38:47.607322    4236 main.go:122] Node: ly-xjf-r021702-gyt ======================
+I1128 20:38:47.638086    4236 main.go:128] kube-controller-manager-ly-xjf-r021702-gyt
+I1128 20:38:47.679592    4236 main.go:128] kube-scheduler-ly-xjf-r021702-gyt
+I1128 20:38:47.682520    4236 main.go:128] cni-delivery-2b7q5
+I1128 20:38:47.755765    4236 main.go:128] etcd-ly-xjf-r021702-gyt
+I1128 20:38:47.798734    4236 main.go:128] kube-apiserver-ly-xjf-r021702-gyt
+I1128 20:38:47.812405    4236 main.go:128] coredns-74676bfdf5-49z74
+I1128 20:38:47.818752    4236 main.go:128] kube-proxy-llv96
+I1128 20:38:47.820706    4236 main.go:122] Node: ly-xjf-r021703-gyt ======================
+I1128 20:38:47.822170    4236 main.go:128] cni-delivery-5qnpb
+I1128 20:38:47.837796    4236 main.go:128] kube-proxy-n5zcs
+I1128 20:38:47.899808    4236 main.go:122] Node: ly-xjf-r021704-gyt ======================
+I1128 20:38:47.910550    4236 main.go:128] cni-delivery-9xgh4
+I1128 20:38:47.943785    4236 main.go:128] coredns-flink-776bb66868-fcwjd
+I1128 20:38:47.945252    4236 main.go:128] kube-proxy-5shwz
 ```
